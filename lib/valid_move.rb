d5cb77d0c67a8e0458c35bdcd177667be4e0b0e2
Austin Williams <austinq.williams@gmail.com>
2019-02-25 22:05:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
- board[index] == " " || board[index] == "" || board[index] == nil
+if  board[index] == " " || board[index] == "" || board[index] == nil
   
   return true 
   
