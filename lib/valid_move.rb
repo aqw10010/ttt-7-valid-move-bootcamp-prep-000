9e2e277aeab1a40a217bcea3cc6a947f6a10589e
Austin Williams <austinq.williams@gmail.com>
2019-02-25 21:31:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,6 +12,8 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken?(board, index)
-  
-end 
+if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
