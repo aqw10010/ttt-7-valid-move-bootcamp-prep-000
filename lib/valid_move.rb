5b8c542e7603a68ea0c65219de747360d1fd39b4
Austin Williams <austinq.williams@gmail.com>
2019-02-25 22:15:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,3 +18,20 @@ end
 
 def position_taken?(board, index)
 end 
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
